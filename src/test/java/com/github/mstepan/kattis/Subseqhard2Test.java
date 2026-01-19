58db5015a463b4d3cd9095914ac2d311a0d9bdd1
Maksym Stepanenko <stepanenkomaksi@gmail.com>
2026-01-19 11:23:11 +0200

    Unit test slightly changed

--- src/test/java/com/github/mstepan/kattis/Subseqhard2Test.java
+++ src/test/java/com/github/mstepan/kattis/Subseqhard2Test.java
@@ -1,29 +1,30 @@
 package com.github.mstepan.kattis;
 
+import static com.github.mstepan.kattis.Subseqhard2.countInterestingSequences;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-class SubseqhardTest {
+class Subseqhard2Test {
 
     @Test
     void singleElementEqualsTarget() {
         int[] arr = {47};
-        int res = Subseqhard.countInterestingSequences(arr);
+        int res = countInterestingSequences(arr);
         assertEquals(1, res);
     }
 
     @Test
     void singleElementNotTarget() {
         int[] arr = {46};
-        int res = Subseqhard.countInterestingSequences(arr);
+        int res = countInterestingSequences(arr);
         assertEquals(0, res);
     }
 
     @Test
     void noSubarrayMatchesTarget() {
         int[] arr = {1, 2, 3};
-        int res = Subseqhard.countInterestingSequences(arr);
+        int res = countInterestingSequences(arr);
         assertEquals(0, res);
     }
 
@@ -33,7 +34,7 @@ class SubseqhardTest {
         // Matching contiguous subarrays that sum to 47:
         // [20,27], [47], [47,0], [0,47], [47] => total 5
         int[] arr = {20, 27, 47, 0, 47};
-        int res = Subseqhard.countInterestingSequences(arr);
+        int res = countInterestingSequences(arr);
         assertEquals(5, res);
     }
 
@@ -41,7 +42,7 @@ class SubseqhardTest {
     void handlesNegativeNumbers() {
         // [10, -10, 47] -> [10,-10,47], [47] => 2
         int[] arr = {10, -10, 47};
-        int res = Subseqhard.countInterestingSequences(arr);
+        int res = countInterestingSequences(arr);
         assertEquals(2, res);
     }
 
@@ -53,14 +54,14 @@ class SubseqhardTest {
         // [1,46,1,-1]
         // total 9
         int[] arr = {1, 46, 1, -1, 47, 0, 0};
-        int res = Subseqhard.countInterestingSequences(arr);
+        int res = countInterestingSequences(arr);
         assertEquals(9, res);
     }
 
     @Test
     void largeValuesDoNotOverflow() {
         int[] arr = {Integer.MAX_VALUE, -Integer.MAX_VALUE + 47};
-        int res = Subseqhard.countInterestingSequences(arr);
+        int res = countInterestingSequences(arr);
         assertEquals(1, res);
     }
 }
